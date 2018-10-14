df827a7889bf415aac98315393d27a14fc51ad59
EvanSimon <evansimon07@gmail.com>
2018-10-14 18:15:59 -0400

    Lab5 factorial

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class MainClass
+{
+
+
+    public static void Main(string[] args)
+
+    {
+        int i;
+        long userNum, result;
+        bool exitProg = false;
+        while (exitProg == false)
+        {
+            Console.WriteLine("enter a number from 1 to 20");
+            userNum = int.Parse(Console.ReadLine());
+            result = userNum;
+
+            if (userNum <= 20)
+
+                for (i = 1; i < userNum; i++)
+                {
+
+                    result = result * i;
+
+                    Console.WriteLine("{0}", result);
+                }
+
+            else if (userNum > 20)
+
+            {
+                Console.WriteLine("Number has to be under 20!!!!");
+            }
+
+           
+            
+                Console.WriteLine("Would you like to continue y/n ?");
+                string exit = Console.ReadLine();
+                if (exit == "y" || exit == "Y")
+
+                {
+                    exitProg = false;
+                    continue;
+                }
+
+                else if (exit == "n" || exit == "N")
+                {
+                    exitProg = true;
+                    break;
+                }
+
+
+                else if (exit != "y" || exit != "n" || exit != "Y" || exit != "Y")
+                {
+                Console.WriteLine("Would you like to continue y/n ?");
+                 exit = Console.ReadLine();
+                continue;
+                }
+            
+
+
+        }
+       
+
+
+        
+
+    }
+}
